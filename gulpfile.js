a0cfc2bb9659b0c4fae3554cefc51babe1f01190
Julian <julians300@gmail.com>
2015-05-20 16:28:51 -0500

    cleaned up stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp');
 
-  minifycss = require('gulp-minify-css'),
+  minifycss = require('gulp-minify-css')
+  uglifyjs  = require('gulp-uglify')
   concat    = require('gulp-concat')
   rename    = require('gulp-rename')
   header    = require('gulp-header');
 
   var paths = {
-  stylesheets: ['src/css/*.css']
+  stylesheets: ['src/css/*.css'],
+  javascripts: ['src/js/*.js']
 };
 
 // CSS Tasks - Concat, Minify
 gulp.task('css',function(){
-  return gulp.src(['src/css/info.css', 'src/css/reset.css', 'src/css/*.css'])
+  return gulp.src(['src/css/info.css', 'src/css/reset.css', 'src/css/grid.css', 'src/css/typography.css', 'src/css/navigation.css', 'src/css/buttons.css', 'src/css/forms.css', 'src/css/tables.css', 'src/css/alerts.css', 'src/css/lists.css', 'src/css/utilities.css', 'src/css/*.css'])
   .pipe(concat('lion.css'))
   .pipe(gulp.dest('./'))
   .pipe(minifycss())
@@ -20,10 +22,22 @@ gulp.task('css',function(){
   .pipe(gulp.dest('./'))
 })
 
-// Watch Task - Watch All CSS Files
-gulp.task('watch', function() {
-  gulp.watch(paths.stylesheets, ['css'])
-});
+// JS Tasks - Uglify
+gulp.task('js',function(){
+  return gulp.src(['src/js/lion.js'])
+  .pipe(gulp.dest('./'))
+  .pipe(uglifyjs())
+  .pipe(rename('lion.min.js'))
+  .pipe(header('/* Lion Framework - Compiled on <%= new Date() %> */ \n'))
+  .pipe(gulp.dest('./'))
+})
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'css']);
+gulp.task('default', ['css', 'js']);
+
+// Watch Task - Watch All CSS and JS Files
+gulp.task('watch', function() {
+  gulp.watch(paths.stylesheets, ['css'])
+  gulp.watch(paths.javascripts, ['js'])
+});
+
