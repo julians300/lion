f4c8ec6d4c6271e208bfff34953c58c438617281
Julian <julians300@gmail.com>
2018-07-04 21:17:55 -0500

    1.5 updates

---
+++ docs/example/home2.php
@@ -0,0 +1,168 @@
+<?php include 'config.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lion Responsive Framework</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/lion.min.css">
+  <link rel="stylesheet" href="../css/custom.css">
+</head>
+<body>
+  <div class="container">
+    <nav class="navbar p-t p-b">
+      <div class="navbar-brand">
+        <a class="navbar-item hamburger">
+          <span class="navbar-hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-menu-left">
+          <a class="navbar-item" href="#">
+            Logo
+          </a>
+        </div>
+        <div class="navbar-menu-right">
+          <a class="navbar-item" href="#">About</a>
+          <a class="navbar-item" href="#">Services</a>
+          <a class="navbar-item" href="#">Pricing</a>
+          <a class="navbar-item" href="#">Contact</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div class="bg-gradient-blue">
+    <div class="hero">
+      <div>
+        <div class="container">
+            <h1 class="white">A complete business management software.</h1>
+            <div class="m-t-lg">
+              <a href="#" class="button">Sign Up</a>
+              <a href="#" class="button outline">View Demo</a>
+            </div>
+            
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section>
+    <div class="container">
+      <div class="p-b-xxl">
+          <h2>Core Features</h2>
+          <p class="muted">Lorem ipsum dolor sit amet consectetur, consectetur adipisicing elit.</p>
+      </div>
+      <div class="row m-b">
+        <div class="four">
+        <h3>Focus on Sales</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam archi.</p>
+        </div>
+        <div class="four">
+        <h3>Easy Communication</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam.</p>
+        </div>
+        <div class="four">
+        <h3>User Friendly Interface</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="four">
+        <h3>E-Commerce</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div class="four">
+        <h3>Responsive Design</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div class="four">
+        <h3>Web Security</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section">
+    <div class="container">
+        <div>
+          <h2>Testimonials</h2>
+          <p class="muted">Over 1000 satisfied customers.</p>
+        </div>
+      <div class="row m-b-xl">
+        <div class="four">
+          <div class="card has-shadow">
+            <div class="p-a">
+              <div class="b-r">
+                <img src="http://source.unsplash.com/d2MSDujJl2g/400x400" alt="">
+              </div>
+              <div class="m-t center">
+                <h4>Jason Turner</h4>
+                <small class="muted">CEO</small>
+                <div class="m-t-sm">
+                  <img src="../img/stars.svg" class="w-100">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="four">
+          <div class="card has-shadow">
+            <div class="p-a">
+              <div class="b-r">
+                <img src="http://source.unsplash.com/UJm8kDbE57Y/400x400" alt="">
+              </div>
+              <div class="m-t center">
+                <h4>Jason Turner</h4>
+                <small class="muted">CEO</small>
+                <div class="m-t-sm">
+                  <img src="../img/stars.svg" class="w-100">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="four">
+          <div class="card has-shadow">
+            <div class="p-a">
+              <div class="b-r">
+                <img src="http://source.unsplash.com/g4DgCF90EM4/400x400" alt="">
+              </div>
+              <div class="m-t center">
+                <h4>Jason Turner</h4>
+                <small class="muted">CEO</small>
+                <div class="m-t-sm">
+                  <img src="../img/stars.svg" class="w-100">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="pull-right">
+        <button class="button">←</button>
+        <button class="button">→</button>
+      </div>
+    </div>
+  </section>
+
+  <footer class="p-b-lg p-t-lg">
+    <div class="container">
+      <div class="row">
+        <div class="six">
+          <div>Copyright 2018</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="../js/lion.min.js"></script>
+  <script src="../js/scripts.js"></script>
+</body>
+</html>
