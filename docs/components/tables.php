53179bce070b6e4fddea963084aefd19e56bc4ea
Julian <julians300@gmail.com>
2018-07-04 22:16:04 -0500

    responsive tables

--- docs/components/tables.php
+++ docs/components/tables.php
@@ -1,89 +1,93 @@
 <section id="tables">
       <h2>Tables</h2>
       <p>Use semantic markup and it works as well.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>Location</th>
-            <th>Profession</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Jeri Seinfeld</td>
-            <td>32</td>
-            <td>Male</td>
-            <td>New York City</td>
-            <td>Comedian</td>
-          </tr>
-          <tr>
-            <td>George Castanza</td>
-            <td>33</td>
-            <td>Male</td>
-            <td>Queens</td>
-            <td>Architect*</td>
-          </tr>
-          <tr>
-            <td>Elaine Benis</td>
-            <td>32</td>
-            <td>Female</td>
-            <td>New York City</td>
-            <td>Writer</td>
-          </tr>
-          <tr>
-            <td>Cosmo Kramer</td>
-            <td>38</td>
-            <td>Male</td>
-            <td>Next door.</td>
-            <td>The Assman</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Sex</th>
+              <th>Location</th>
+              <th>Profession</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jerry Seinfeld</td>
+              <td>32</td>
+              <td>Male</td>
+              <td>New York City</td>
+              <td>Comedian</td>
+            </tr>
+            <tr>
+              <td>George Castanza</td>
+              <td>33</td>
+              <td>Male</td>
+              <td>Queens</td>
+              <td>Architect*</td>
+            </tr>
+            <tr>
+              <td>Elaine Benis</td>
+              <td>32</td>
+              <td>Female</td>
+              <td>New York City</td>
+              <td>Writer</td>
+            </tr>
+            <tr>
+              <td>Cosmo Kramer</td>
+              <td>38</td>
+              <td>Male</td>
+              <td>Next door.</td>
+              <td>The Assman</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
       <h3>Striped Table</h3>
-      <table class="striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>Location</th>
-            <th>Profession</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Jeri Seinfeld</td>
-            <td>32</td>
-            <td>Male</td>
-            <td>New York City</td>
-            <td>Comedian</td>
-          </tr>
-          <tr>
-            <td>George Castanza</td>
-            <td>33</td>
-            <td>Male</td>
-            <td>Queens</td>
-            <td>Architect*</td>
-          </tr>
-          <tr>
-            <td>Elaine Benis</td>
-            <td>32</td>
-            <td>Female</td>
-            <td>New York City</td>
-            <td>Writer</td>
-          </tr>
-          <tr>
-            <td>Cosmo Kramer</td>
-            <td>38</td>
-            <td>Male</td>
-            <td>Next door.</td>
-            <td>The Assman</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table class="striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Sex</th>
+              <th>Location</th>
+              <th>Profession</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Jerry Seinfeld</td>
+              <td>32</td>
+              <td>Male</td>
+              <td>New York City</td>
+              <td>Comedian</td>
+            </tr>
+            <tr>
+              <td>George Castanza</td>
+              <td>33</td>
+              <td>Male</td>
+              <td>Queens</td>
+              <td>Architect*</td>
+            </tr>
+            <tr>
+              <td>Elaine Benis</td>
+              <td>32</td>
+              <td>Female</td>
+              <td>New York City</td>
+              <td>Writer</td>
+            </tr>
+            <tr>
+              <td>Cosmo Kramer</td>
+              <td>38</td>
+              <td>Male</td>
+              <td>Next door.</td>
+              <td>The Assman</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>  
       <div class="row">
 <pre><code>
 &lt;table&gt;
@@ -110,7 +114,7 @@
   
     &lt;tr&gt;
     
-      &lt;td&gt;Jeri Seinfeld&lt;/td&gt;
+      &lt;td&gt;Jerry Seinfeld&lt;/td&gt;
       
       &lt;td&gt;32&lt;/td&gt;
       
